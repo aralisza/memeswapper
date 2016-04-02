ec146af0a4f2a4a991c3ffadd8c1e59a96d3ca52
Emily Shi <shi.yue@husky.neu.edu>
2016-04-02 19:44:06 -0400

    stuff

--- convert.js
+++ convert.js
@@ -3,6 +3,8 @@ function convert(imgs) {
     while (imgs.length > 0) {
         convertImageToCanvas(imgs[0]);
     }
+
+    overlayImages(30, 50);
 }
 
 // converts the given image to canvas
@@ -18,3 +20,45 @@ function convertImageToCanvas(image) {
 
 var imgs = document.getElementsByTagName("img");
 convert(imgs);
+
+function loadImage(src, onload) {
+    // http://www.thefutureoftheweb.com/blog/image-onload-isnt-being-called
+    var img = new Image();
+    img.src = src;
+    return img;
+}
+
+function resize(img) {
+    var w = img.width;
+    var h = img.height;
+
+    var neww = 200;
+    var newh = 200;
+
+    if (w > h) {
+        var r = neww/w;
+        nh = r * h;
+        img.width = neww;
+        img.height = newh;
+    }
+    else {
+        var r = newh/h;
+        neww = r * w;
+        img.width = neww;
+        img.height = newh;
+    }
+    
+    return img;
+}
+
+function overlayImages(x, y) {
+    var cvs = document.getElementsByTagName("canvas");
+    var toLoad = resize(loadImage('http://introcs.cs.princeton.edu/java/31datatype/peppers.jpg'));
+
+    for (var i=0; i<cvs.length; i++) {
+        cvs[i].getContext("2d").drawImage(toLoad, x, y);
+    }
+
+    //$('canvas').append("<img src='" + img.src + "'height='300', width='300'>");
+    //image.append("<img src='" + imgPath + "' height='300', width='300'>");
+}
