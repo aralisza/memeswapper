d45d7f0a8c4bfed52568e82140d17b7d23c9e7d4
Emily Shi <shi.yue@husky.neu.edu>
2016-04-03 08:56:33 -0400

    kinda works

--- convert.js
+++ convert.js
@@ -1,7 +1,7 @@
 main(loadImage);
 
 function main(load) {
-    load("http://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png", convertTo)
+    load("http://i1214.photobucket.com/albums/cc500/violoncellox/01.png", convertTo)
 }
 
 function loadImage(src, converting) {
@@ -9,25 +9,19 @@ function loadImage(src, converting) {
     var img = new Image();
     img.src = src;
     img.onload = function() {
-        converting(img, imgs, overlayImage);
+        converting(img, imgs, getBounds);
     }
 }
 
-function convertTo(img, imgs, overlay) {
-    for (var i=0; i<imgs.length; i++) {
+function convertTo(img, imgs, getDims) {
+    for (var i=0; i<imgs.length; i+=2) {
         imgs[i].id = "img";
         $(imgs[i]).wrap("<div class='memeWrapper'></div>");
         var targetImage = $(imgs[i]).parent();
-
-        overlay(img, targetImage, imgs[i], getBounds);
+        var p = $(img).wrap("<div class='memeOverlay'></div>").parent();
+        $(targetImage).append(p);
+        getDims(img, targetImage, imgs[i], alignOverlay, resize, p);
     }
-}
-
-function overlayImage(img, img, targetImage, targetConvert, getDims) {
-    var p = $(img).wrap("<div class='memeOverlay'></div>").parent();
-    $(targetImage).append(p);
-
-    getDims(img, targetImage, targetConvert, alignOverlay, resize, p);
 }
 
 function getBounds(img, targetImage, targetConvert, align, resize, p) {
@@ -37,34 +31,40 @@ function getBounds(img, targetImage, targetConvert, align, resize, p) {
     tracker.setStepSize(1.7);
 
     tracking.track('#img', tracker);
-    debugger;
+
     tracker.on('track', function(event) {
-        debugger;
         if (event.data.length === 0) {
-            debugger;
+
             return results;
         }
         event.data.forEach(function(rect) {
-            debugger;
             var arr = [rect.x, rect.y, rect.width, rect.height];
             console.log(arr);
-            results.push(arr);
 
-            var oldw = img.width;
-            var oldh = img.height;
-
-            plz(p, img, oldw, oldh, dims, i, imageToDiv, align, resize);
+            plz(p, img, arr, imageToDiv, align, resize, targetConvert, targetImage);
         });
+        // var rect = event.data[0];
+        // var arr = [rect.x, rect.y, rect.width, rect.height];
+        // console.log(arr);
+        //
+        // plz(p, img, arr, imageToDiv, align, resize, targetConvert, targetImage);
     });
+
+    console.log(results);
 }
 
-function plz(p, img, oldw, oldh, dims, i, imageToDiv, align, resize) {
-    align(resize(p, p, img, oldw, oldh, dims[i][2], dims[i][3]), dims[i][0], dims[i][1]);
+function plz(p, img, dims, toDiv, align, resize, targetConvert, targetImage) {
+
+    align(resize(p, img, dims[2], dims[3]), dims[0], dims[1]);
+    toDiv(targetImage, targetConvert);
 }
 
-function resize(modDiv, img, oldw, oldh, nw, nh) {
+function resize(modDiv, img, nw, nh) {
+
     var nimg = $(img);
     var md = $(modDiv);
+    var oldw = img.width;
+    var oldh = img.height;
 
 
     if (oldw > oldh) {
@@ -95,4 +95,19 @@ function alignOverlay(md, x, y) {
     md.css("left", x + "px");
     md.css("top", y + "px");
     md.css("position", "absolute");
+}
+
+function imageToDiv(targetImage, targetConvert) {
+    var md = $(targetImage);
+
+    md.css("width", targetConvert.width);
+    md.css("height", targetConvert.height);
+    md.css("background-image", "url('" + targetConvert.src + "')");
+    md.css("background-size", targetConvert.width + "px " + targetConvert.height + "px");
+    md.css("background-repeat", "no-repeat");
+    md.css("position", "relative");
+    md.css("display", "inline-block");
+    debugger;
+    targetConvert.width = 1;
+    targetConvert.height = 1;
 }
